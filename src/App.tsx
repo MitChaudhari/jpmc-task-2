9ea77de9ff8188b218facd700c3068eea7692643
Mitansh Chaudhari <93405512+MitChaudhari@users.noreply.github.com>
2023-01-13 18:50:42 -0600

    Updated the App.tsx
    Accordingly to the guide I finished updating the app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import './App.css';
  */
 interface IState {
   data: ServerRespond[],
+  showGraph: boolean, // We want the graph to show when the user commands
 }
 
 /**
@@ -22,13 +23,14 @@ class App extends Component<{}, IState> {
       // data saves the server responds.
       // We use this state to parse data down to the child element (Graph) as element property
       data: [],
+      showGraph: false, // we are defining the initial state of the graph as hidden
     };
   }
 
   /**
    * Render Graph react component with state.data parse as property data
    */
-  renderGraph() {
+   renderGraph() {
     return (<Graph data={this.state.data}/>)
   }
 
@@ -36,11 +38,19 @@ class App extends Component<{}, IState> {
    * Get new data from server and update the state with the new data
    */
   getDataFromServer() {
-    DataStreamer.getData((serverResponds: ServerRespond[]) => {
+    let x = 0;
+    const interval = setInterval(() => {
+        DataStreamer.getData((serverResponds: ServerRespond[]) => {
       // Update the state by creating a new array of data that consists of
       // Previous data in the state and the new data from server
-      this.setState({ data: [...this.state.data, ...serverResponds] });
-    });
+            this.setState({ data: serverResponds, showGraph: true, });
+        });
+        x++;
+        if (x > 1000) {
+         clearInterval(interval);
+        }
+    }, 100);
+    
   }
 
   /**
